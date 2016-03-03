d5d1749a573c5a467700a83f5863846cad07c8a0
Catzaizai <catzaizai@live.com>
2016-03-04 00:31:34 +0800

    add index

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,3 +1,84 @@
 /**
  * Created by 猫崽崽 on 1/3/2016.
  */
+
+(function(window, document, $, undefined){
+
+    var Loading = function(element, option){
+        this.loadHtml = "";
+        this.$load = null;
+        this.$element = $(element);
+        this.command = option.command;
+
+        this._init();
+    };
+
+    Loading.prototype = {
+        constructor: Loading,
+
+        _init: function(){
+            this.loadHtml='<div class="loading">' +
+                '<div class="spinner">' +
+                '<div class="rect1"></div>' +
+                '<div class="rect2"></div>' +
+                '<div class="rect3"></div>' +
+                '<div class="rect4"></div>' +
+                '<div class="rect5"></div>' +
+                '</div>' +
+                '</div>';
+            if(!this.$element.find(".loading")[0]){
+                this.$element.append(this.loadHtml);
+            }
+            this.$load = this.$element.find(".loading");
+
+            this.execute();
+        },
+
+        execute: function(){
+            switch (this.command){
+                case "show":
+                    this.show();
+                    this.command = "hide";
+                    break;
+                case "hide":
+                    this.hide();
+                    this.command = "show";
+                    break;
+                default:
+                    break;
+            }
+        },
+
+        show: function(){
+            this.$load.show();
+        },
+
+        hide: function(){
+            this.$load.hide();
+        }
+    };
+
+    $.fn.loading = function(option){
+        return this.each(function(){
+            var $this = $(this);
+            var data = $this.data("loading");
+            var param = { };
+            if(typeof option == "string"){
+                param = { command : option};
+            }
+            if(!data){
+                data = new Loading(this, $.extend($.fn.loading.default, param));
+            }else{
+                data = $.extend(data,$.fn.loading.default, param);
+                data.$element = $this;
+                data.execute();
+            }
+            $this.data("loading", data);
+        });
+    };
+
+    $.fn.loading.default = {
+        command: "show"
+    }
+
+})(window, document, jQuery);
